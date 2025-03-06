5cf7c15d64566ce900b49694d0731ea4098d18a2
A-Monner <amonner@acornlibrary.org>
2025-03-06 14:39:18 -0600

    Login Page Constructing (Part 5)

--- login.php
+++ login.php
@@ -5,6 +5,16 @@
     </head>
 
     <body>
-        <div id="login_header">MyBook Signup<div>
+        <div id="login_header">
+            <div id="MyBook">MyBook</div>
+            <div id="signup_button">Signup</div>
+        </div>
+
+        <div id="login_box">
+            <div>Login to MyBook</div><br>
+            <input type="text" id="text"><br><br>
+            <input type="text" id="text"><br><br>
+            <input type="submit" id="button" value="Login">
+        </div>
     </body>
 </html>
