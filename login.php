1fd6ba5f01eae00a5c4308f0cacdace1eceb6093
A-Monner <amonner@acornlibrary.org>
2025-03-06 13:59:48 -0600

    Created Login page HTML (Part 2)

---
+++ login.php
@@ -0,0 +1,10 @@
+<html>
+    <head>
+        <title>MyBook</title>
+        <link href="css/login_styles.css" rel="stylesheet">
+    </head>
+
+    <body>
+        <div id="login_header">MyBook Signup<div>
+    </body>
+</html>
