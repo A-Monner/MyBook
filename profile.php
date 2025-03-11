53a3909a44cf40ae22c59c0d9468e2d34b802a46
A-Monner <amonner@acornlibrary.org>
2025-03-11 14:15:09 -0500

    Designing Profile Page (Part 10)

---
+++ profile.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>MyBook | Profile</title>
+        <link href="css/profile_styles.css" rel="stylesheet">
+    </head>
+
+    <body>
+
+        <!-- Top Bar -->
+        <div id="top-bar">
+            <div id="site_name">
+                MyBook &nbsp &nbsp
+                <input type="text" id="search_box" placeholder="Search for people">
+                <img src="images/selfie.jpg" id="corner-pfp">
+            </div>
+        </div>
+
+        <!-- Cover Area -->
+         <div id="cover-area">
+            <div id="cover-top">
+                <img src="images/mountain.jpg" id="cover-image">
+                <img src="images/selfie.jpg" id="profile_pic"><br>
+                <div id="profile_name">Mary Banda</div><br>
+                <div id="menu-buttons">Timeline</div>
+                <div id="menu-buttons">About</div>
+                <div id="menu-buttons">Friends</div>
+                <div id="menu-buttons">Photos</div>
+                <div id="menu-buttons">Settings</div>
+            </div>
+
+            <div id="content-area">
+                <div id="friends-bar"></div>
+                <div id="posts-bar"></div>
+            </div>
+
+         </div>
+    </body>
+</html>
