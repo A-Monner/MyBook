4c99687cb46d267a4c1e8404c408f6f009cf8885
A-Monner <amonner@acornlibrary.org>
2025-03-12 14:03:28 -0500

    Finished Profile Page (Part 13)

--- profile.php
+++ profile.php
@@ -29,9 +29,65 @@
                 <div id="menu-buttons">Settings</div>
             </div>
 
+            <!-- Below Cover Area -->
             <div id="content-area">
-                <div id="friends-bar"></div>
-                <div id="posts-bar"></div>
+
+                <!-- Friends Area -->
+                <div id="friends-area">
+                    <div id="friends-bar">
+                        Friends<br>
+                        <div id="friends">
+                            <img src="images/user1.jpg" id="friends-img"><br>
+                            First User
+                        </div>
+
+                        <div id="friends">
+                            <img src="images/user2.jpg" id="friends-img"><br>
+                            Second User
+                        </div>
+
+                        <div id="friends">
+                            <img src="images/user3.jpg" id="friends-img"><br>
+                            Third User
+                        </div>
+
+                        <div id="friends">
+                            <img src="images/user4.jpg" id="friends-img"><br>
+                            Fourth User
+                        </div>
+                    </div>
+                </div>
+
+                <!-- Posts Area -->
+                <div id="posts-area">
+                    <div id="posts-bar">
+                        <textarea id="post-form" placeholder="What's on your mind?"></textarea>
+                        <input id="post-submit" type="submit" value="Post"><br>
+                    </div>
+
+                    <!-- Posts -->
+                    <div id="timeline-bar">
+                        <div id="post">
+                            <div>
+                                <img src="images/user1.jpg" id="post-pic">
+                            </div>
+                            <div>
+                                <div id="poster_name">First Guy</div>
+                                Lorem Ipsum is simply dummy text of the printing and typesetting
+                                industry. Lorem Ipsum has been the industry's standard dummy
+                                text ever since the 1500s, when an unknown printer took a galley
+                                of type and scrambled it to make a type specimen book. It has
+                                survived not only five centuries, but also the leap into electronic
+                                typesetting, remaining essentially unchanged. It was popularised in
+                                the 1960s with the release of Letraset sheets containing Lorem
+                                Ipsum passages, and more recently with desktop publishing
+                                software like Aldus PageMaker including versions of Lorem Ipsum.
+                                <br><br>
+                                <a href="">Like</a> . <a href="">Comment</a> . <span>April 23 2025<span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
 
          </div>
